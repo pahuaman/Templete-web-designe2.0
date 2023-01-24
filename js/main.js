ccc180c7bdafe4d352ebe32f6203f9e857a4e16b
José Garfia Pahua <mediobestia_275@hotmail.com>
2023-01-24 12:19:18 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+    
+    const ham = document.querySelector('.ham');
+
+    const enlaces = document.querySelector('.enlaces-menu');
+
+    ham.addEventListener('click', () => {
+        
+        enlaces.classList.toggle('activado');
+        
+    });
+
+});//ready
